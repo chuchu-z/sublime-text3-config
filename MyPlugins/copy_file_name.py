03d1e900bede0c6acf69ee57b4d414cbc7c58f5b
zhou <1813713320@qq.com>
2020-11-11 09:27:52 +0800

    add_copy_file_name

---
+++ MyPlugins/copy_file_name.py
@@ -0,0 +1,12 @@
+import sublime
+import sublime_plugin
+import os
+
+
+class copyfilenameCommand(sublime_plugin.TextCommand):
+
+    def run(self, edit):
+        file_name = self.view.file_name()
+        base_name = os.path.basename(file_name)
+        sublime.set_clipboard(base_name)
+        # print('get_clipboard: ' + sublime.get_clipboard())
